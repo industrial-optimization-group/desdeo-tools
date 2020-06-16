361255bfc3c52304c0868b00e9d0dbe3cedf675b
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2020-06-17 01:44:39 +0300

    Fixed path in sphinx conf file.

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../desdeo-tools"))
+sys.path.insert(0, os.path.abspath("../desdeo_tools"))
 
 
 # -- Project information -----------------------------------------------------
