fb04a9b0da6ed80a26c5c8e34a576dd0da406835
Antti Luopajärvi <amluopav@student.jyu.fi>
2020-09-15 21:11:40 +0300

    Test method without epsilons (objective functions not to be minimized) with ScalarMinimizer.

--- desdeo_tools/scalarization/EpsilonConstraintMethod.py
+++ desdeo_tools/scalarization/EpsilonConstraintMethod.py
@@ -7,34 +7,30 @@ class EpsilonConstraintMethod:
     """A class to represent a class for scalarizing MOO problems using the epsilon
         constraint method.
     Attributes:
-        epsilons (np.ndarray): The epsilon values to set as the upper limit
-        for each objective when treated as a constraint.
         to_be_minimized (int): Integer representing which objective function
         should be minimized.
     """
 
     def __init__(
-            self, epsilons: np.ndarray, to_be_minimized: int
+            self, to_be_minimized: int
     ):
         """
 
         Args:
-            epsilons (np.ndarray): The epsilon values to set as the upper limit
-            for each objective when treated as a constraint.
             to_be_minimized (int): integer representing which objective function
             should be minimized.
         """
 
-        self._epsilons = epsilons
         self._to_be_minimized = to_be_minimized
 
     def __call__(self, xs: np.ndarray) -> np.ndarray:
-        return xs[self._to_be_minimized]
+        return xs[0][self._to_be_minimized]
+
+    # TODO include epsilons (other objective functions)
 
 
-
-""" 
-FOR TESTING THE METHOD
+"""
+TESTING THE METHOD
 """
 if __name__ == "__main__":
     def volume(r, h):
@@ -52,7 +48,6 @@ if __name__ == "__main__":
 
 
     # bounds
-
     r_bounds = np.array([2.5, 15])
     h_bounds = np.array([10, 50])
     bounds = np.stack((r_bounds, h_bounds))
@@ -67,15 +62,15 @@ if __name__ == "__main__":
         return -(xs[:, 0] / xs[:, 1] - 1.618)
 
 
-    def simple_sum(xs):
-        xs = np.atleast_2d(xs)
-        return np.sum(xs, axis=1)
+    obj_min = 0
 
-    eps = EpsilonConstraintMethod(epsilons=[],to_be_minimized=0)
+    eps = EpsilonConstraintMethod(obj_min)
+
     scalarized_objective = Scalarizer(objective, eps)
     print(scalarized_objective)
 
     minimizer = ScalarMinimizer(scalarized_objective, bounds, constraint_evaluator=con_golden, method=None)
+
     x0 = np.array([2.6, 11])
     sum_res = minimizer.minimize(x0)
     print(sum_res)
