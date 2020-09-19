c5b734dcc86231f9acce9333c1568e4aa95d20fe
Antti Luopajärvi <amluopav@student.jyu.fi>
2020-09-19 16:37:21 +0300

    Add epsilons to method

--- desdeo_tools/scalarization/EpsilonConstraintMethod.py
+++ desdeo_tools/scalarization/EpsilonConstraintMethod.py
@@ -1,32 +1,68 @@
 import numpy as np
 from desdeo_tools.scalarization import Scalarizer
 from desdeo_tools.solver.ScalarSolver import ScalarMinimizer
+from typing import Optional, Callable
 
 
 class EpsilonConstraintMethod:
     """A class to represent a class for scalarizing MOO problems using the epsilon
         constraint method.
     Attributes:
+        objective_vector (np.ndarray): Values of objective functions.
         to_be_minimized (int): Integer representing which objective function
         should be minimized.
+        epsilons (np.ndarray): Upper bounds chosen by the decison maker.
+        constraints (Optional[Callable]): Other constraints, if existing.
     """
 
     def __init__(
-            self, to_be_minimized: int
+            self, objective_vector: np.ndarray, to_be_minimized: int, epsilons: np.ndarray,
+            constraints: Optional[Callable]
     ):
         """
 
         Args:
+            constraints (object):
             to_be_minimized (int): integer representing which objective function
             should be minimized.
+            epsilons (np.ndarray): Upper bounds chosen by the decison maker.
+            constraints (Optional[Callable]): Other constraints, if existing.
         """
-
+        self.objective_vector = objective_vector
         self._to_be_minimized = to_be_minimized
+        self.epsilons = epsilons
+        self.constraints = constraints
 
-    def __call__(self, xs: np.ndarray) -> np.ndarray:
-        return xs[0][self._to_be_minimized]
+    def cons(self, xs) -> np.ndarray:
+        """
+        Returns values of constraints
+        Args:
+            xs (np.ndarray): Decision variables
 
-    # TODO include epsilons (other objective functions)
+        Returns: Values of constraint functions (both "original" constraints as well as epsilon constraints) in a vector.
+
+        """
+        xs = np.atleast_2d(xs)
+        if self.constraints:
+            c = self.constraints(xs)
+        else:
+            c = []
+        # remove objective to be minimized
+        eps_functions = np.array(
+            [self.objective_vector[0][i] for i in range(len(self.objective_vector[0])) if i != self._to_be_minimized])
+        e = np.array([-(f - v) for f, v in zip(eps_functions, self.epsilons)])
+        return np.concatenate([c, e])
+
+    def __call__(self, objective_vector: np.ndarray) -> float:
+        """
+        Returns the value of objective function to be minimized.
+        Args:
+            objective_vector (np.ndarray): Values of objective functions.
+
+        Returns: Value of objective function to be minimized.
+
+        """
+        return objective_vector[0][self._to_be_minimized]
 
 
 """
@@ -63,14 +99,26 @@ if __name__ == "__main__":
 
 
     obj_min = 0
-
-    eps = EpsilonConstraintMethod(obj_min)
+    epsil = np.array([-1000])
+    x0 = np.array([2.6, 11])
+    o = objective(x0)
+    xs = np.atleast_2d(x0)
+    cons = -(xs[:, 0] / xs[:, 1] - 1.618)
+    eps = EpsilonConstraintMethod(o, obj_min, epsil, con_golden)
+    c = eps.cons(x0)
+    print(c)
 
     scalarized_objective = Scalarizer(objective, eps)
     print(scalarized_objective)
 
-    minimizer = ScalarMinimizer(scalarized_objective, bounds, constraint_evaluator=con_golden, method=None)
+    minimizer = ScalarMinimizer(scalarized_objective, bounds, constraint_evaluator=eps.cons, method=None)
 
-    x0 = np.array([2.6, 11])
-    sum_res = minimizer.minimize(x0)
-    print(sum_res)
+    res = minimizer.minimize(x0)
+    final_r, final_h = res["x"][0], res["x"][1]
+    final_obj = objective(res["x"]).squeeze()
+    final_V, final_A = final_obj[0], final_obj[1]
+
+    print(f"Final cake specs: radius: {final_r}cm, height: {final_h}cm.")
+    print(f"Final cake dimensions: volume: {final_V}, area: {-final_A}.")
+    print(final_r / final_h)
+    print(res)
