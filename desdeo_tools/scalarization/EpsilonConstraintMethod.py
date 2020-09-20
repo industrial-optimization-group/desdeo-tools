ec229480e017683a0bdf4422e038651330b96861
Antti Luopajärvi <amluopav@student.jyu.fi>
2020-09-20 17:10:33 +0300

    Improvements, add error handling

--- desdeo_tools/scalarization/EpsilonConstraintMethod.py
+++ desdeo_tools/scalarization/EpsilonConstraintMethod.py
@@ -1,60 +1,70 @@
 import numpy as np
+from numpy.core._multiarray_umath import ndarray
+
 from desdeo_tools.scalarization import Scalarizer
 from desdeo_tools.solver.ScalarSolver import ScalarMinimizer
 from typing import Optional, Callable
+
+
+class ECMError(Exception):
+    """Raised when an error related to the Epsilon Constraint Method is encountered.
+
+    """
 
 
 class EpsilonConstraintMethod:
     """A class to represent a class for scalarizing MOO problems using the epsilon
         constraint method.
     Attributes:
-        objective_vector (np.ndarray): Values of objective functions.
+        objectives (Callable): Objective functions.
         to_be_minimized (int): Integer representing which objective function
         should be minimized.
         epsilons (np.ndarray): Upper bounds chosen by the decison maker.
-        constraints (Optional[Callable]): Other constraints, if existing.
+                               Epsilon constraint functions are defined in a following form:
+                                    f_i(x) <= eps_i
+                               If the constraint function is of form
+                                    f_i(x) >= eps_i
+                               Remember to multiply the epsilon value with -1!
+        constraints (Optional[Callable]): Function that returns definitions of other constraints, if existing.
     """
 
     def __init__(
-            self, obj_fun: Callable, objective_vector: np.ndarray, to_be_minimized: int, epsilons: np.ndarray,
+            self, objectives: Callable, to_be_minimized: int, epsilons: np.ndarray,
             constraints: Optional[Callable]
     ):
-        """
-
-        Args:
-            constraints (object):
-            to_be_minimized (int): integer representing which objective function
-            should be minimized.
-            epsilons (np.ndarray): Upper bounds chosen by the decison maker.
-            constraints (Optional[Callable]): Other constraints, if existing.
-        """
-        self.obj_fun = obj_fun
-        self.objective_vector = objective_vector
+        self.objectives = objectives
         self._to_be_minimized = to_be_minimized
         self.epsilons = epsilons
         self.constraints = constraints
 
-    def get_constraints(self, xs) -> np.ndarray:
+    def evaluate_constraints(self, xs) -> np.ndarray:
         """
-        Returns values of constraints
+        Returns values of constraints with given decison variables.
         Args:
-            xs (np.ndarray): Decision variables
+            xs (np.ndarray): Decision variables.
 
         Returns: Values of constraint functions (both "original" constraints as well as epsilon constraints) in a vector.
 
         """
         xs = np.atleast_2d(xs)
+
+        # evaluate epsilon constraint function "left-side" values with given decision variables
+        epsilon_left_side = np.array(
+            [self.objectives(xs)[0][i] for i, _ in enumerate(self.objectives(xs)[0]) if i != self._to_be_minimized])
+
+        if len(epsilon_left_side) != len(self.epsilons):
+            msg = ("The lenght of the epsilons array ({}) must match the total number of objectives - 1 ({})."
+                   ).format(len(self.epsilons), len(self.objectives(xs)[0]) - 1)
+            raise ECMError(msg)
+
+        # evaluate values of epsilon constraint functions
+        e: ndarray = np.array([-(f - v) for f, v in zip(epsilon_left_side, self.epsilons)])
+
         if self.constraints:
             c = self.constraints(xs)
+            return np.concatenate([c, e])
         else:
-            c = []
-        # epsilon function values with current decision variables
-        eps_functions = np.array(
-            [self.obj_fun(xs)[0][i] for i in range(len(self.objective_vector[0])) if i != self._to_be_minimized])
-
-        # epsilon constraint values
-        e = np.array([-(f - v) for f, v in zip(eps_functions, self.epsilons)])
-        return np.concatenate([c, e])
+            return e
 
     def __call__(self, objective_vector: np.ndarray) -> float:
         """
@@ -68,10 +78,10 @@ class EpsilonConstraintMethod:
         return objective_vector[0][self._to_be_minimized]
 
 
-"""
-TESTING THE METHOD
-"""
+# Testing the method
 if __name__ == "__main__":
+    # 1. Define objective functions, bounds and constraints
+
     def volume(r, h):
         return np.pi * r ** 2 * h
 
@@ -80,20 +90,24 @@ if __name__ == "__main__":
         return 2 * np.pi ** 2 + np.pi * r * h
 
 
+    # add third objective
+    def weight(v):
+        return 0.01 * v
+
+
     def objective(xs):
         # xs is a 2d array like, which has different values for r and h on its first and second columns respectively.
         xs = np.atleast_2d(xs)
-        return np.stack((volume(xs[:, 0], xs[:, 1]), -area(xs[:, 0], xs[:, 1]))).T
+        return np.stack((volume(xs[:, 0], xs[:, 1]), -area(xs[:, 0], xs[:, 1]), weight(volume(xs[:, 0], xs[:, 1])))).T
 
 
-    # bounds
+    # bounds for decision variables
     r_bounds = np.array([2.5, 15])
     h_bounds = np.array([10, 50])
     bounds = np.stack((r_bounds, h_bounds))
 
 
     # constraints
-
     def con_golden(xs):
         # constraints are defined in DESDEO in a way were a positive value indicates an agreement with a constraint, and
         # a negative one a disagreement.
@@ -101,27 +115,38 @@ if __name__ == "__main__":
         return -(xs[:, 0] / xs[:, 1] - 1.618)
 
 
-    obj_min = 0
-    epsil = np.array([-2000])
-    x0 = np.array([10, 11])
-    o = objective(x0)
-    xs = np.atleast_2d(x0)
-    cons = -(xs[:, 0] / xs[:, 1] - 1.618)
-    eps = EpsilonConstraintMethod(objective, o, obj_min, epsil, con_golden)
-    c = eps.get_constraints(x0)
-    print(c)
+    # 2. Apply Epsilon contraint method
 
+    # index of which objective function to minimize
+    obj_min = 2
+
+    # set upper bound(s) for the other objectives, in the same order than which corresponding objective functions
+    # are defined
+    epsil = np.array([2000, -100])  # multiply the epsilons with -1, if the constraint is of form f_i(x) >= e_i
+
+    # create an instance of EpsilonConstraintMethod-class for given problem
+    eps = EpsilonConstraintMethod(objective, obj_min, epsil, constraints=con_golden)
+
+    # constraint evaluator, used by the solver
+    cons_evaluate = eps.evaluate_constraints
+
+    # scalarize
     scalarized_objective = Scalarizer(objective, eps)
     print(scalarized_objective)
 
-    minimizer = ScalarMinimizer(scalarized_objective, bounds, constraint_evaluator=eps.get_constraints, method=None)
+    # 3. Solve
 
+    # starting point
+    x0 = np.array([2, 11])
+    minimizer = ScalarMinimizer(scalarized_objective, bounds, constraint_evaluator=cons_evaluate, method=None)
+
+    # minimize
     res = minimizer.minimize(x0)
     final_r, final_h = res["x"][0], res["x"][1]
     final_obj = objective(res["x"]).squeeze()
-    final_V, final_A = final_obj[0], final_obj[1]
+    final_V, final_A, final_W = final_obj[0], final_obj[1], final_obj[2]
 
     print(f"Final cake specs: radius: {final_r}cm, height: {final_h}cm.")
-    print(f"Final cake dimensions: volume: {final_V}, area: {-final_A}.")
+    print(f"Final cake dimensions: volume: {final_V}, area: {-final_A}, weight: {final_W}.")
     print(final_r / final_h)
     print(res)
