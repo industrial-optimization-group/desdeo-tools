103b8b447bd4a39fdba1460ac6490e3f1efbc10c
Giomara Lárraga Maldonado <glarragw@it-adam-2012.it.jyu.fi>
2021-02-12 03:08:26 +0200

    New module for scalarization functions for MOEAD

--- desdeo_tools/scalarization/MOEADSF.py
+++ desdeo_tools/scalarization/MOEADSF.py
@@ -20,7 +20,7 @@ class MOEADSFBase(abc.ABC):
     """
 
     @abstractmethod
-    def __call__(self, objective_vector: np.ndarray, weight_vector: np.ndarray, ideal_vector: np.ndarray) -> Union[float, np.ndarray]:
+    def __call__(self, objective_vector: np.ndarray, weight_vector: np.ndarray, reference_point: np.ndarray) -> Union[float, np.ndarray]:
         """Evaluate the SF.
 
         Args:
@@ -28,7 +28,7 @@ class MOEADSFBase(abc.ABC):
             the values.
             weight_vectors (np.ndarray): The weight vector to calculate the
             values.
-
+            reference_point (np.ndarray): The reference point (i.e nadir or ideal vector).
         Returns:
             Union[float, np.ndarray]: Either a single SF value or a vector of
             values if objective is a 2D array.
@@ -40,14 +40,14 @@ class Tchebycheff(MOEADSFBase):
     """Implements the Tchebycheff scalarizing function.
     """
 
-    def __call__(self, objective_vector: np.ndarray, weight_vector: np.ndarray, ideal_vector: np.ndarray) -> Union[float, np.ndarray]:
+    def __call__(self, objective_vector: np.ndarray, weight_vector: np.ndarray, reference_point: np.ndarray) -> Union[float, np.ndarray]:
         """Evaluate the Tchebycheff scalarizing function.
 
         Args:
             objective_vector (np.ndarray): A vector representing a solution in
-            the solution space.
-            weight_vector (np.ndarray): 
-            ideal_vector (np.ndarray): 
+            the objective space.
+            weight_vector (np.ndarray): A weight vector representing the direction
+            reference_point (np.ndarray): A reference point (i.e the ideal vector or the nadir vector)
         Raises:
             MOEADSFError: The dimensions of the objective vector and weight_vector don't match.
 
@@ -57,11 +57,11 @@ class Tchebycheff(MOEADSFBase):
         """
         if not objective_vector.shape == weight_vector.shape:
             msg = ("The dimensions of the objective vector {} and " "weight_vector {} do not match.").format(
-                objective_vector, weight_vector
+                objective_vector.shape, weight_vector.shape
             )
             raise MOEADSFError(msg)
 
-        feval   = np.abs(objective_vector - ideal_vector) * weight_vector
+        feval   = np.abs(objective_vector - reference_point) * weight_vector
         max_fun = np.max(feval)
         return  max_fun
 
@@ -69,21 +69,39 @@ class Tchebycheff(MOEADSFBase):
 class WeightedSum(MOEADSFBase):
     """Implements the Weighted sum scalarization function
     """
-    def __call__(self, objective_vector: np.ndarray, weight_vector: np.ndarray, ideal_vector = None) -> Union[float, np.ndarray]:
+    def __call__(self, objective_vector: np.ndarray, weight_vector: np.ndarray, reference_point = None) -> Union[float, np.ndarray]:
+        """Evaluate the WeightedSum scalarizing function.
+
+        Args:
+            objective_vector (np.ndarray): A vector representing a solution in
+            the objective space.
+            weight_vector (np.ndarray): A weight vector representing the direction
+        Raises:
+            MOEADSFError: The dimensions of the objective vector and weight_vector don't match.
+
+        Note:
+            The shaped of objective_vector and weight_vector must match. The reference point is not needed.
+
+        """
+        if not objective_vector.shape == weight_vector.shape:
+            msg = ("The dimensions of the objective vector {} and " "weight_vector {} do not match.").format(
+                objective_vector.shape, weight_vector.shape
+            )
+            raise MOEADSFError(msg)
         feval   = np.sum(objective_vector * weight_vector)
         return feval
 
 
 class PBI(MOEADSFBase):
     """Implements the PBI scalarization function
-
     Args:
-        ideal (np.ndarray): The ideal point.
-        theta(float): 
+        theta(float): A penalty parameter used by the function
 
     Attributes:
-        ideal (np.ndarray): The ideal point.
-        theta (float): 
+        theta (float): A penalty parameter used by the function
+
+    .. Q. Zhang and H. Li, "MOEA/D: A Multiobjective Evolutionary Algorithm Based on Decomposition," 
+       in IEEE Transactions on Evolutionary Computation, vol. 11, no. 6, pp. 712-731, Dec. 2007, doi: 10.1109/TEVC.2007.892759.
     """
 
     def __init__(
@@ -92,19 +110,38 @@ class PBI(MOEADSFBase):
     ):
         self.theta = theta
 
-    def __call__(self, objective_vector: np.ndarray, weight_vector: np.ndarray, ideal_vector: np.ndarray) -> Union[float, np.ndarray]:
-        norm_weights    = np.linalg.norm(weight_vector)
-        weights         = weight_vector/norm_weights
-        fx_a            = objective_vector - ideal_vector
-        d1              = np.inner(fx_a, weights)
+    def __call__(self, objective_vector: np.ndarray, weight_vector: np.ndarray, reference_point: np.ndarray) -> Union[float, np.ndarray]:
+        """Evaluate the PBI scalarizing function.
 
-        fx_b            = objective_vector - (ideal_vector + d1 * weights)
+        Args:
+            objective_vector (np.ndarray): A vector representing a solution in
+            the objective space.
+            weight_vector (np.ndarray): A weight vector representing the direction
+            reference_point (np.ndarray): A reference point (i.e the ideal vector or the nadir vector)
+        Raises:
+            MOEADSFError: The dimensions of the objective vector and weight_vector don't match.
+
+        Note:
+            The shaped of objective_vector and weight_vector must match. The reference point is not needed.
+
+        """
+        if not objective_vector.shape == weight_vector.shape:
+            msg = ("The dimensions of the objective vector {} and " "weight_vector {} do not match.").format(
+                objective_vector.shape, weight_vector.shape
+            )
+            raise MOEADSFError(msg)
+
+        norm_weights    = np.linalg.norm(weight_vector)
+        weights         = np.true_divide(weight_vector, norm_weights)
+        fx_a            = objective_vector - reference_point
+        d1              = np.fabs(np.inner(fx_a, weights))
+
+        fx_b            = objective_vector - (reference_point + d1 * weights)
         d2              = np.linalg.norm(fx_b)
+        
         
         fvalue          = d1 + self.theta * d2
         return fvalue
-
-
 
 
 
