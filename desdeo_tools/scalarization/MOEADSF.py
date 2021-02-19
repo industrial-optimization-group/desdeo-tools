5e3baa793cc23d77cf02667a54d901ba08e2bf54
Giomara Lárraga Maldonado <glarragw@it-adam-2012.it.jyu.fi>
2021-02-19 20:58:59 +0200

    unification of terms and change of format

--- desdeo_tools/scalarization/MOEADSF.py
+++ desdeo_tools/scalarization/MOEADSF.py
@@ -20,15 +20,22 @@ class MOEADSFBase(abc.ABC):
     """
 
     @abstractmethod
-    def __call__(self, objective_vector: np.ndarray, weight_vector: np.ndarray, reference_point: np.ndarray) -> Union[float, np.ndarray]:
+    def __call__(
+        self,
+        objective_vector: np.ndarray,
+        reference_vector: np.ndarray,
+        ideal_vector: np.ndarray,
+        nadir_vector: np.ndarray,
+    ) -> Union[float, np.ndarray]:
         """Evaluate the SF.
 
         Args:
-            objective_vectors (np.ndarray): The objective vectors to calculate
+            objective_vector (np.ndarray): The objective vector to calculate
             the values.
-            weight_vectors (np.ndarray): The weight vector to calculate the
+            reference_vector (np.ndarray): The reference vector to calculate the
             values.
-            reference_point (np.ndarray): The reference point (i.e nadir or ideal vector).
+            ideal_vector (np.ndarray): The ideal objective vector.
+            nadir_vector (np.ndarray): The nadir objective vector.
         Returns:
             Union[float, np.ndarray]: Either a single SF value or a vector of
             values if objective is a 2D array.
@@ -40,55 +47,65 @@ class Tchebycheff(MOEADSFBase):
     """Implements the Tchebycheff scalarizing function.
     """
 
-    def __call__(self, objective_vector: np.ndarray, weight_vector: np.ndarray, reference_point: np.ndarray) -> Union[float, np.ndarray]:
-        """Evaluate the Tchebycheff scalarizing function.
+    def __call__(
+        self,
+        objective_vector: np.ndarray,
+        reference_vector: np.ndarray,
+        ideal_vector: np.ndarray,
+    ) -> Union[float, np.ndarray]:
+        """Evaluate the Tchebycheff scalarizing function for minimization problems.
 
         Args:
             objective_vector (np.ndarray): A vector representing a solution in
             the objective space.
-            weight_vector (np.ndarray): A weight vector representing the direction
-            reference_point (np.ndarray): A reference point (i.e the ideal vector or the nadir vector)
+            reference_vector (np.ndarray): A reference vector representing the direction
+            ideal_vector (np.ndarray): The ideal objective vector 
         Raises:
-            MOEADSFError: The dimensions of the objective vector and weight_vector don't match.
+            MOEADSFError: The dimensions of the objective vector and reference_vector don't match.
 
         Note:
-            The shaped of objective_vector and weight_vector must match.
+            The shaped of objective_vector and reference_vector must match.
 
         """
-        if not objective_vector.shape == weight_vector.shape:
-            msg = ("The dimensions of the objective vector {} and " "weight_vector {} do not match.").format(
-                objective_vector.shape, weight_vector.shape
-            )
+        if not objective_vector.shape == reference_vector.shape:
+            msg = (
+                "The dimensions of the objective vector {} and "
+                "reference_vector {} do not match."
+            ).format(objective_vector.shape, reference_vector.shape)
             raise MOEADSFError(msg)
 
-        feval   = np.abs(objective_vector - reference_point) * weight_vector
+        feval = np.abs(objective_vector - ideal_vector) * reference_vector
         max_fun = np.max(feval)
-        return  max_fun
+        return max_fun
 
 
 class WeightedSum(MOEADSFBase):
     """Implements the Weighted sum scalarization function
     """
-    def __call__(self, objective_vector: np.ndarray, weight_vector: np.ndarray, reference_point = None) -> Union[float, np.ndarray]:
+
+    def __call__(
+        self, objective_vector: np.ndarray, reference_vector: np.ndarray
+    ) -> Union[float, np.ndarray]:
         """Evaluate the WeightedSum scalarizing function.
 
         Args:
             objective_vector (np.ndarray): A vector representing a solution in
             the objective space.
-            weight_vector (np.ndarray): A weight vector representing the direction
+            reference_vector (np.ndarray): A reference vector representing the direction
         Raises:
-            MOEADSFError: The dimensions of the objective vector and weight_vector don't match.
+            MOEADSFError: The dimensions of the objective vector and reference_vector don't match.
 
         Note:
-            The shaped of objective_vector and weight_vector must match. The reference point is not needed.
+            The shaped of objective_vector and reference_vector must match. A reference point is not needed.
 
         """
-        if not objective_vector.shape == weight_vector.shape:
-            msg = ("The dimensions of the objective vector {} and " "weight_vector {} do not match.").format(
-                objective_vector.shape, weight_vector.shape
-            )
+        if not objective_vector.shape == reference_vector.shape:
+            msg = (
+                "The dimensions of the objective vector {} and "
+                "reference_vector {} do not match."
+            ).format(objective_vector.shape, reference_vector.shape)
             raise MOEADSFError(msg)
-        feval   = np.sum(objective_vector * weight_vector)
+        feval = np.sum(objective_vector * reference_vector)
         return feval
 
 
@@ -104,44 +121,44 @@ class PBI(MOEADSFBase):
        in IEEE Transactions on Evolutionary Computation, vol. 11, no. 6, pp. 712-731, Dec. 2007, doi: 10.1109/TEVC.2007.892759.
     """
 
-    def __init__(
-        self,
-        theta: float = 5
-    ):
+    def __init__(self, theta: float = 5):
         self.theta = theta
 
-    def __call__(self, objective_vector: np.ndarray, weight_vector: np.ndarray, reference_point: np.ndarray) -> Union[float, np.ndarray]:
-        """Evaluate the PBI scalarizing function.
+    def __call__(
+        self,
+        objective_vector: np.ndarray,
+        reference_vector: np.ndarray,
+        ideal_vector: np.ndarray,
+    ) -> Union[float, np.ndarray]:
+        """Evaluate the PBI scalarizing function for minimization problems.
 
         Args:
             objective_vector (np.ndarray): A vector representing a solution in
             the objective space.
-            weight_vector (np.ndarray): A weight vector representing the direction
-            reference_point (np.ndarray): A reference point (i.e the ideal vector or the nadir vector)
+            reference_vector (np.ndarray): A reference vector representing the direction
+            ideal_vector (np.ndarray): The ideal objecive vector
         Raises:
-            MOEADSFError: The dimensions of the objective vector and weight_vector don't match.
+            MOEADSFError: The dimensions of the objective vector and reference_vector don't match.
 
         Note:
-            The shaped of objective_vector and weight_vector must match. The reference point is not needed.
+            The shaped of objective_vector and reference_vector must match. The reference point is not needed.
 
         """
-        if not objective_vector.shape == weight_vector.shape:
-            msg = ("The dimensions of the objective vector {} and " "weight_vector {} do not match.").format(
-                objective_vector.shape, weight_vector.shape
-            )
+        if not objective_vector.shape == reference_vector.shape:
+            msg = (
+                "The dimensions of the objective vector {} and "
+                "reference_vector {} do not match."
+            ).format(objective_vector.shape, reference_vector.shape)
             raise MOEADSFError(msg)
 
-        norm_weights    = np.linalg.norm(weight_vector)
-        weights         = np.true_divide(weight_vector, norm_weights)
-        fx_a            = objective_vector - reference_point
-        d1              = np.fabs(np.inner(fx_a, weights))
+        norm_weights = np.linalg.norm(reference_vector)
+        weights = np.true_divide(reference_vector, norm_weights)
+        fx_a = objective_vector - ideal_vector
+        d1 = np.fabs(np.inner(fx_a, weights))
 
-        fx_b            = objective_vector - (reference_point + d1 * weights)
-        d2              = np.linalg.norm(fx_b)
-        
-        
-        fvalue          = d1 + self.theta * d2
+        fx_b = objective_vector - (ideal_vector + d1 * weights)
+        d2 = np.linalg.norm(fx_b)
+
+        fvalue = d1 + self.theta * d2
         return fvalue
-
-
 
