ae91c64aa1a5e87dfedf8f1f5fff84889fd515c1
jpajasmaa <juuso.pajasmaa@gmail.com>
2021-10-12 10:14:54 +0300

    changes to comments

--- desdeo_tools/utilities/quality_indicator.py
+++ desdeo_tools/utilities/quality_indicator.py
@@ -5,51 +5,45 @@ from desdeo_tools.scalarization import SimpleASF
 
 
 @njit()
-def epsilon_indicator(reference_front: np.ndarray, front: np.ndarray) -> float:
-    """ Computes the additive epsilon-indicator between reference front and current approximating front.
-
+def epsilon_indicator(s1: np.ndarray, s2: np.ndarray) -> float:
+    """ Computes the additive epsilon-indicator between two solutions.
     Args:
-        reference_front (np.ndarray): The reference front that the current front is being compared to.
-        Should be an one-dimensional array.
-        front (np.ndarray): The front that is compared. Should be one-dimensional array with the same shape as
-        reference_front.
+        s1 (np.ndarray): Solution 1. Should be an one-dimensional array.
+        s2 (np.ndarray): Solution 2. Should be an one-dimensional array.
 
     Returns:
-        float: The factor by which the approximating front is worse than the reference front with respect to all
-        objectives.
+        float: The factor by which the first solution is worse than the other solution.
     """
     eps = 0.0
-    for i in range(reference_front.size):
-        value = front[i] - reference_front[i]
+    for i in range(s1.size):
+        value = s2[i] - s1[i]
         if value > eps:
             eps = value
     return eps
 
 
-def epsilon_indicator_ndims(reference_front: np.ndarray, front: np.ndarray) -> list:
+def epsilon_indicator_ndims(front: np.ndarray, reference_point: np.ndarray) -> list:
     """ Computes the additive epsilon-indicator between reference front and current one-dimensional vector of front.
     Args:
-        reference_front (np.ndarray): The reference front that the current front is being compared to.
+        front (np.ndarray): The front that the current reference point is being compared to.
         Should be set of arrays, where the rows are the solutions and the columns are the objective dimensions.
-        front (np.ndarray): The front that is compared. Should be one-dimensional array.
+        reference_point (np.ndarray): The reference point that is compared. Should be one-dimensional array.
     Returns:
-        float: The factor by which the approximating front is worse than the reference front with respect to all
+        float: The factor by which the approximating front is worse than the reference point with respect to all
         objectives.
     """
-    eps_list = np.array(np.zeros(reference_front.shape[0]))
-    for i in np.arange(reference_front.shape[0]):
-        eps_list[i] = np.max(front - reference_front[i])
+    eps_list = np.array(np.zeros(front.shape[0]))
+    for i in np.arange(front.shape[0]):
+        eps_list[i] = np.max(reference_point - front[i])
     return eps_list
 
 
-def preference_indicator(reference_front: np.ndarray, front: np.ndarray, min_asf_value: float, ref_point: np.ndarray, delta: float) -> float:
+def preference_indicator(s1: np.ndarray, s2: np.ndarray, min_asf_value: float, ref_point: np.ndarray, delta: float) -> float:
     """ Computes the preference-based quality indicator.
 
     Args:
-        reference_front (np.ndarray): The reference front that the current front is being compared to.
-        Should be an one-dimensional array.
-        front (np.ndarray): The front that is compared. Should be one-dimensional array with the same shape as
-        reference_front.
+        s1 (np.ndarray): Solution 1. Should be an one-dimensional array.
+        s2 (np.ndarray): Solution 2. Should be an one-dimensional array.
         ref_point (np.ndarray): The reference point should be same shape as front.
         min_asf_value (float): Minimum value of achievement scalarization of the reference_front. Used in normalization.
         delta (float): The spesifity delta allows to set the amplification of the indicator to be closer or farther 
@@ -57,28 +51,29 @@ def preference_indicator(reference_front: np.ndarray, front: np.ndarray, min_asf
         point.
 
     Returns:
-        float: The factor by which the approximating front is worse than the reference front with respect to all
-        objectives taking into account the reference point given and spesifity.
+        float: The factor by which the first solution is worse than the other solution taking into account 
+        the reference point given and spesifity.
     """
-    front_asf = SimpleASF(np.ones_like(front))
-    norm = front_asf(front, reference_point=ref_point) + delta - min_asf_value
-    return epsilon_indicator(reference_front, front) / norm
+    s2_asf = SimpleASF(np.ones_like(s2))
+    norm = s2_asf(s2, reference_point=ref_point) + delta - min_asf_value
+    return epsilon_indicator(s1, s2) / norm
 
 
-def hypervolume_indicator(reference_front: np.ndarray, front: np.ndarray) -> float:
+def hypervolume_indicator(front: np.ndarray, reference_point: np.ndarray) -> float:
     """ Computes the hypervolume-indicator between reference front and current approximating point.
 
     Args:
-        reference_front (np.ndarray): The reference front that the current front is being compared to.
-        Should be set of arrays, where the rows are the solutions and the columns are the objective dimensions.
-        front (np.ndarray): The front that is compared. Should be 1D array.
+        front (np.ndarray): The front that is compared. Should be set of arrays, where the rows are the solutions and 
+        the columns are the objective dimensions.
+        reference_point (np.ndarray): The reference point that the current front is being compared to. 
+        Should be 1D array.
 
     Returns:
         float: Measures the volume of the objective space dominated by an approximation set.
     """
-    ref = np.asarray(reference_front, dtype='double') # hv.wfg needs datatype to be double
+    ref = np.asarray(reference_point, dtype='double') # hv.wfg needs datatype to be double
     fr = np.asarray(front, dtype='double')
-    return hv.wfg(ref, fr)
+    return hv.wfg(fr, ref)
 
 
 if __name__=="__main__":
@@ -86,6 +81,7 @@ if __name__=="__main__":
     po_front = np.asarray([[1.0,0],[0.5,0.5], [0,1.0], [2, -1], [0,0]])
     sol1 = [2,2] # cant be better than po front, min is zero
     sol = np.asarray(sol1)
+    ref = np.asarray([0.7, 0.3])
 
     print("eps indi value")
     print(epsilon_indicator(po_front[0], sol))
@@ -96,4 +92,5 @@ if __name__=="__main__":
     print("ndims")
     print(epsilon_indicator_ndims(po_front, sol))
     print(hypervolume_indicator(po_front, sol))
-
+    print("pref")
+    print(preference_indicator(po_front[1], sol, 0.1, ref, 0.1))
