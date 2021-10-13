47c0f605c1b4894338910ab28ebda2737b637d14
Aswin Murali <aswinmurali.co@gmail.com>
2021-10-13 09:22:50 +0530

    formatted scalarization init file docs

--- desdeo_tools/scalarization/__init__.py
+++ desdeo_tools/scalarization/__init__.py
@@ -1,8 +1,6 @@
 """This module implements methods for defining functions to scalarize vector valued functions.
-These are knows as `Scalarizer`s.
-It also provides achievement
-scalarizing functions to be used with the scalarizers.
-
+These are knows as `Scalarizer`s. It also provides achievement scalarizing functions to be used
+with the scalarizers.
 """
 
 __all__ = [
@@ -38,6 +36,7 @@ from desdeo_tools.scalarization.ASF import (
     StomASF,
     GuessASF,
 )
+
 from desdeo_tools.scalarization.Scalarizer import DiscreteScalarizer, Scalarizer
 from desdeo_tools.scalarization.MOEADSF import Tchebycheff, WeightedSum, PBI
 
